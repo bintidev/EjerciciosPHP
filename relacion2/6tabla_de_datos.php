976e35df5133caeac2731cbcecee0212db886fd8
B. <bd90962@gmail.com>
2025-10-07 20:38:15 +0200

    Modificacion super-index y Realizacion de Ejercicios 6 y 7

---
+++ relacion2/6tabla_de_datos.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 6 - Tabla de Datos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    
+    <h1>Estilizando una Tabla con Github</h1>
+
+    <table class="table table-danger table-striped-columns table-hover">
+        <tr>
+            <td>Nombre</td>
+            <td>Apellido</td>
+            <td>Email</td>
+            <td>Teléfono</td>
+        </tr>
+        <tr>
+            <td>Gerard</td>
+            <td>Sunley</td>
+            <td>gsunley0@deliciousdays.com</td>
+            <td>998 621 7245</td>
+        </tr>
+        <tr>
+            <td>Hadley</td>
+            <td>Hagstone</td>
+            <td>hhagstone1@joomla.org</td>
+            <td>761 589 1065</td>
+        </tr>
+        <tr>
+            <td>Cordula</td>
+            <td>Messier</td>
+            <td>cmessier2@mysql.com</td>
+            <td>795 863 5554</td>
+        </tr>
+        <tr>
+            <td>Olenka</td>
+            <td>Teacy</td>
+            <td>oteacy3@dedecms.com</td>
+            <td>269 885 0435</td>
+        </tr>
+    </table>
+
+</body>
+</html>
