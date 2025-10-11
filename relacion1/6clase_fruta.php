6234164ead381da5bdc9583a2366adf399f81606
B. <bd90962@gmail.com>
2025-10-11 13:46:16 +0200

    Modificada la Ruta de Favicon

--- relacion1/6clase_fruta.php
+++ relacion1/6clase_fruta.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="./playamar.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/img/playamar.png" type="image/x-icon">
     <title>Ejercicio 6 - Clase Fruta</title>
 </head>
 <body>
