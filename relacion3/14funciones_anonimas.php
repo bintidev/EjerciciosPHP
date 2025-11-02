a66af316de61aed5b76031505c9e5fb2347f5668
B. <bd90962@gmail.com>
2025-11-02 18:49:45 +0100

    Comenzando Ejercicio 14

---
+++ relacion3/14funciones_anonimas.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 14 - Funciones Anónimas</title>
+    <link rel="shortcut icon" href="../assets/img/playamar.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        #wrapper {
+            height: 80vh;
+        }
+
+        #radioHelp {
+            visibility: hidden;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="d-flex justify-content-center align-items-center" id="wrapper">
+        <form class="p-3 shadow rounded bg-secondary-subtle">
+            <h1 class="text-primary">Cálculos a Partir de Radio</h1>
+            <div class="mb-3">
+                <label for="numero1" class="form-label">Introduce el radio:</label>
+                <input class="form-control" type="number" placeholder="Como mucho 2 cifras decimales" aria-label="default input example">
+                <div class="text-danger" id="radioHelp"></div>
+            </div>
+
+            <div class="mb-3">
+                <button type="submit" class="btn btn-outline-primary">Submit</button>
+            </div>
+        </form>
+    </div>
+
+    <div class="container-fluid w-75">
+
+        <p class="text-center fs-4">
+
+            <?php
+            ?>
+
+        </p>
+
+    </div>
+
+</body>
+</html>
